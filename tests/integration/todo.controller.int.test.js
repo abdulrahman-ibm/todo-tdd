4c36f9c5a15d9eb1cc1861664edb48c945aa3b01
Abdulrahman Yussuf <abdulrahmanyussuf@AbdulrahmansMBP.lan>
2021-03-13 22:45:26 +0000

    Add integration test for todoController.getTodos

--- tests/integration/todo.controller.int.test.js
+++ tests/integration/todo.controller.int.test.js
@@ -5,6 +5,15 @@ const endpointUrl = "/todos/"
 const newTodo = require("../mock-data/new-todo.json")
 
 describe(endpointUrl, () => {
+    it("GET " + endpointUrl, async () => {
+        const response = await request(app).get(endpointUrl)
+
+        expect(response.statusCode).toBe(200)
+        expect(Array.isArray(response.body)).toBeTruthy()
+        expect(response.body[0].title).toBeDefined()
+        expect(response.body[0].done).toBeDefined()
+    })
+
     it("POST " + endpointUrl, async () => {
         const response = await request(app)
         .post(endpointUrl)
