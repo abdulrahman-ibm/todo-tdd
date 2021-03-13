f8d59acc4a54b08e57e008a70213ab7d59b6f5d0
Abdulrahman Yussuf <abdulrahmanyussuf@AbdulrahmansMBP.lan>
2021-03-13 22:36:31 +0000

    Add error handling tests for TodoController.getTodos

--- controllers/todo.controller.js
+++ controllers/todo.controller.js
@@ -3,8 +3,17 @@ const TodoModel = require('../model/todo.model')
 exports.createTodo = async (req, res, next) => {
     try {
         const createModel = await TodoModel.create(req.body)
-        res.status(201).json(createModel)  
-    } catch(err) {
+        res.status(201).json(createModel)
+    } catch (err) {
         next(err)
     }
 };
+
+exports.getTodos = async (req, res, next) => {
+    try {
+        const allTodos = await TodoModel.find({})
+        res.status(200).json(allTodos)
+    } catch (err) {
+        next(err);
+    }
+}
